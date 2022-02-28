3d89d89601320ab7e8262bf91b508dfe31e97c99
zyl740176835 <61375483+zyl740176835@users.noreply.github.com>
2022-02-28 23:20:43 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,5 +12,5 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate;
-find package/ -type d -name luci-app-easymesh -exec rm -rf {} \;
-git clone https://github.com/zyl740176835/luci-app-easymesh package/luci-app-easymesh
+find  feeds/luci/applications/ -name luci-app-easymesh -exec rm -rf {} \;
+git clone https://github.com/zyl740176835/luci-app-easymesh feeds/luci/applications/luci-app-easymesh
